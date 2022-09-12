2494b2afea392039e4b11a2ccb66e89acca17ced
Mark Lwembe <marklwembe1@gmail.com>
2022-09-12 23:42:00 +0300

    Correction: Added new line char

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,14 +1,12 @@
 #include<stdio.h>
-
 /**
  * main - Entry point
  *
  * Return: Always 0 (Success)
  */
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
 
- int main (void)
- {
-	printf("_putchar");
-
-	return 0;
- }
