9059b2d974608746fdbe7600d7e8d52103a4fb77
Mark Lwembe <marklwembe1@gmail.com>
2022-09-13 17:25:07 +0300

    Update

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -10,10 +10,10 @@ int main(void)
 	long int t1 = 1, t2 = 2;
 	long int nextTerm = t1 + t2;
 
-	printf("%ld, %ld, ", t1, t2);
+	printf("%lu, %lu, ", t1, t2);
 	for (i = 3; i <= 98; ++i)
 	{
-		printf("%ld, ", nextTerm);
+		printf("%lu, ", nextTerm);
 		t1 = t2;
 		t2 = nextTerm;
 		nextTerm = t1 + t2;
