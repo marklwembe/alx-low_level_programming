4c49e21406f4fc7bc90f18e7aa3f7ccae7b4628c
Mark Lwembe <marklwembe1@gmail.com>
2022-09-13 15:23:05 +0300

    Fix error betty style error

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,16 +1,17 @@
 #include "main.h"
 /**
- * Prints the alphabet.
- *
- * No return value
+ * print_alphabet - It prints the latin alphabet
+ * @void - Takes no arguments
+ * Return: void
  */
 void print_alphabet(void)
 {
 	int i = 0;
 
-        while (i < 26) {
+	while (i < 26)
+	{
 			_putchar('a' + i);
 			++i;
-		}
-		_putchar('\n');
+	}
+	_putchar('\n');
 }
