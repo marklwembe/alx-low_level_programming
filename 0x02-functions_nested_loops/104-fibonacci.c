37cb57e5323b128f76cb031ae77c870dd21e5d1e
Mark Lwembe <82794352+marklwembe@users.noreply.github.com>
2022-09-13 16:49:29 +0300

    Create 104-fibonacci.c

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,17 @@
+
+int main(void)
+{
+	int i, n;
+	int t1 = 1, t2 = 2;
+	int nextTerm = t1 + t2;
+
+	printf("%d, %d, ", t1, t2);
+	for (i = 3; i <= n; ++i)
+	{
+		printf("%d, ", nextTerm);
+		t1 = t2;
+		t2 = nextTerm;
+		nextTerm = t1 + t2;
+	}
+	  return 0;
+}
