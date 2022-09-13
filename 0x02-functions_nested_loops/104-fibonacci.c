dcc7f2a33df7957947f4ea79c3cfbb7b595b1621
Mark Lwembe <marklwembe1@gmail.com>
2022-09-13 17:40:39 +0300

    Update 2

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -7,8 +7,8 @@
 int main(void)
 {
 	int i;
-	long long int t1 = 1, t2 = 2e;
-	long long int nextTerm = t1 + t2;
+	unsigned long int t1 = 1, t2 = 2e;
+	unsigned long int nextTerm = t1 + t2;
 
 	printf("%llu, %llu, ", t1, t2);
 	for (i = 3; i <= 98; ++i)
