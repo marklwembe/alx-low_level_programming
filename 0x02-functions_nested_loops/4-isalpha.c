eeec37077b0d62ea2306105e88a315c93f102da9
Mark Lwembe <marklwembe1@gmail.com>
2022-09-15 00:27:50 +0300

    Task 4. isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isalpha - Checks if character is a letter
+ * @c: integer input variable
+ * Return: 2 if letter 0 otherwise
+ */
+
+int _isalpha(int c)
+{
+	if ((c > 64 && c < 91)) || (c > 96 && c <123)
+		return (1);
+	else
+		return (0);
+}
+
