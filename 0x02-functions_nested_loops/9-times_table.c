388376529a745031322aefbe29f1dcbc2f1e514c
markrazorx <marklwembe1@gmail.com>
2022-09-15 02:39:29 +0300

    Update 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -29,8 +29,8 @@ void times_table(void)
 			{
 				_putchar(',');
 				_putchar(' ');
-				_putchar(result / 10 + '0');
-				_putchar(result % 10 + '0');
+				_putchar((result / 10) + '0');
+				_putchar((result % 10) + '0');
 			}
 			++b;
 		}
