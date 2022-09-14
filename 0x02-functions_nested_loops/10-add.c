57c1494b13700bf99d78c811afd544a54e586133
markrazorx <marklwembe1@gmail.com>
2022-09-15 02:58:49 +0300

    Task 10. a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * add _ Adds to numbers and retuns the sum
+ * @a: First number
+ * @b: Second number
+ * Return: The sum of the two inputs
+ */
+
+int add(int a, int b)
+{
+	int result = a + b;
+	return (result);
+}
