ebe18584eaa70ca105e777670a45a2fcd310200f
markrazorx <marklwembe1@gmail.com>
2022-09-15 01:32:57 +0300

    Task 6. Absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _abs - Calculates absolute value of given number
+ * @n: Integer number given 
+ * Return: absolute of number given
+ */
+
+ int _abs(int n) {
+	if (c > 0)
+		return (c);
+	else
+		return (-c);
+ }
