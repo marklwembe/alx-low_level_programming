71e4f0cffff237e102503c6d4486037f4d63419e
markrazorx <marklwembe1@gmail.com>
2022-09-15 01:19:49 +0300

    Update 5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -6,8 +6,8 @@
  * Return: 1 if positive, 0 if zero, -1 if negative
  */
 
- int print_sign(int n)
- {
+int print_sign(int n)
+{
 	if (n > 0)
 	{
 		_putchar('+');
@@ -23,4 +23,4 @@
 		_putchar(0);
 		return (0);
 	}
- }
+}
