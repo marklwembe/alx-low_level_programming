68ed79c6080613b80da3e592e4e82de047196d02
markrazorx <marklwembe1@gmail.com>
2022-09-15 05:16:15 +0300

    Task 16. 100 Fibonacci's, 1 and 2 first.

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -6,17 +6,32 @@
  */
 int main(void)
 {
-	int i;
-	unsigned long int t1 = 1, t2 = 2;
-	unsigned long int nextTerm = t1 + t2;
+	unsigned long int i, term_1, term_2, term_1_div, term_2_div, result_1, result_2;
 
-	printf("%lu, %lu, ", t1, t2);
-	for (i = 3; i <= 98; ++i)
+	term_1 = 1;
+	term_2 = 2;
+
+	printf("%lu", term_1);
+	for (i = 1; i < 91; i++)
 	{
-		printf("%lu, ", nextTerm);
-		t1 = t2;
-		t2 = nextTerm;
-		nextTerm = t1 + t2;
+		printf(", %lu", term_2);
+		term_2 = term_2 + term_1;
+		term_1 = term_2 - term_1;
+	}
+
+	term_1_div = term_1 / 1000000000;
+	term_2_div = term_1 % 1000000000;
+	result_1 = term_2 / 1000000000;
+	result_2 = term_2 % 1000000000;
+
+	for (i = 92; i < 99; ++i)
+	{
+		printf(", %lu", result_1 + (result_2 / 1000000000));
+		printf("%lu", result_2 % 1000000000);
+		result_1 = result_1 + term_1_div;
+		term_1_div = result_1 - term_1_div;
+		result_2 = result_2 + term_2_div;
+		term_2_div = result_2 - term_2_div;
 	}
 	printf("\n");
 	return (0);
