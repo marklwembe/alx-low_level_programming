64e87d4ecf257635dd753fd3cd7ff8d0097c6c9e
markrazorx <marklwembe1@gmail.com>
2022-09-15 04:47:05 +0300

    Task 15. Even Fibonacci sum till no

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,1 +1,22 @@
 #include <stdio.h>
+/**
+ * main - Prints sum of even Fibonacci nums till 4,000,000
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i = 0;
+	long int t1 = 1, t2 = 2, t3, even_sum;
+
+	while ( t1 <= 4000000)
+	{
+		if (t1 % 2 == 0)
+			even_sum += t1;
+		t3 = t1 + t2;
+		t1 = t2;
+		t2 = t3;
+	}
+	printf("%ld\n", even_sum);
+	return (0);
+}
