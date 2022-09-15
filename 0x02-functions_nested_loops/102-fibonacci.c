f2231695f8b17055ca750fcef1da376a21f0b5d3
markrazorx <marklwembe1@gmail.com>
2022-09-15 04:23:43 +0300

    Task 14. 50 Fibonacci numbers second number 2

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - Prints first 50 fibonacci numbers start at 1 then 2
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i = 0;
+	long int t1 = 1, t2 =2, sum;
+	
+	printf("%ld, %ld, ", t1, t2);
+	while (i < 50)
+	{
+		if (i != 49)
+			printf("%ld, ", t1);
+		else
+			printf("%ld\n", t1);
+		sum = t1 + t2;
+		t1 = t2;
+		t2 = sum;
+		++i;
+	}
+	return (0);
+}
