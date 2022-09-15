f8aeb0e2a4886b7fe48f6325e375af302d0d574d
markrazorx <marklwembe1@gmail.com>
2022-09-15 04:02:13 +0300

    Task 13. Fizz Buzz numbers sum till 1024

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Calculates fizz buzz numbers' sum till 1024
+ * Return: 0 -success
+ */
+
+int main(void)
+{
+	long int result = 0;
+	int i = 0;
+
+	while (i < 1024)
+	{
+		if((i % 3) == 0)
+			result = result + i;
+		else if ((i % 5) == 0)
+			result = result + i;
+		++i;
+	}
+	printf("%ld\n", result);
+
+	return (0);
+}
