793590a66f824fba941f3e86d1c8c70c7e6658e7
markrazorx <marklwembe1@gmail.com>
2022-09-15 03:41:04 +0300

    Task 12. Generate times tables for 0 to 15

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,48 @@
+#include "main.h"
+/**
+ * print_times_table - Print times table of the number given
+ * @n: The number given
+ * Returns: void
+ */
+void print_times_table(int n)
+{
+	int a, b, result;
+
+	if (n >= 0 && < 15)
+	{
+		for (a = 0; a <= n; a++)
+		{
+			for (b = 0; b < =n; b++)
+			{
+				result = b * a;
+				if (b == 0)
+					_putchar(result + '0');
+				else if (result < 10 && b != 0)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(result + '0');
+				}
+				else if (result >= 10 && result < 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar((result / 10) + '0');
+					_putchar((result % 10) + '0');
+				}
+				else if (result >= 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar((k / 100) + '0');
+					_putchar(((k / 10) % 10) + '0');
+					_putchar((k % 10) + '0');
+				}
+			}
+			_putchar('\n');
+		}
+	}
+}
