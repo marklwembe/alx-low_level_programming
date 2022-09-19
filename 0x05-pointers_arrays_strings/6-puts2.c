5e20c7a44fbd949d30ecb49b6ccb39d4adfe31a4
marklwembe <marklwembe1@gmail.com>
2022-09-20 02:40:29 +0300

    Task 6. Print every other character in a string

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * puts2 - Prints one char out of 2 of a string.
+ * @str: pointer to given string
+ * Return: nothing
+ */
+void puts2(char *str)
+{
+	int count = 0;
+
+	while (count >= 0)
+	{
+		if (str[count] == '\0')
+		{
+			putchar('\n');
+			break;
+		}
+		if (count % 2 == 0)
+			putchar(str[count]);
+		count++;
+	}
+}
