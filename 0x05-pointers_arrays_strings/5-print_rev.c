5351be7761e48e95311dd0829f2a360287b5cd06
marklwembe <marklwembe1@gmail.com>
2022-09-20 00:52:02 +0300

    Task 4. Function to print string in reverse

---
+++ 0x05-pointers_arrays_strings/5-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_rev - prints a string in reverse
+ * @s: pointer to string
+ * Return: nothing
+ */
+
+void print_rev(char *s);
+{
+	int count = 0;
+
+	while (s[count] != '\0')
+	{
+		++count;
+	}
+
+	while (count >= 0)
+	{
+		_putchar(str[count - 1]);
+		--count;
+	}
+	_putchar('\n');
+}
