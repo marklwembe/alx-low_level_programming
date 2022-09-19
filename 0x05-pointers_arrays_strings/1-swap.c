d51af5469f5cb87ba6266e6a276a3f42ab6f60a2
marklwembe <marklwembe1@gmail.com>
2022-09-19 22:00:18 +0300

    Task 1. Swapping integer values

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+/**
+ * swap_int - Swaps the values of two integers
+ * @a: first integer
+ * @b: second integer
+ * Returns : nothing
+ */
+
+void swap_int(int *a, int *b)
+{
+	int c = a;
+	*a = b;
+	*b = c;
+}
