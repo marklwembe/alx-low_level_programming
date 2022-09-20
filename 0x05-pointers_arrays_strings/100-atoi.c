c1024bc7a33896d89e9e5e14ea22112991782ef3
marklwembe <marklwembe1@gmail.com>
2022-09-20 04:27:00 +0300

    Task 10. Function that converts a string to an int

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * _atoi - converts a string to an integer.
+ * @s: input string.
+ * Return: A
+ */
+int _atoi(char *s)
+{
+	unsigned int count = 0, size = 0, oi = 0, pn = 1, m = 1, i;
+
+	while (*(s + count) != '\0')
+	{
+		if (size > 0 && (*(s + count) < '0' || *(s + count) > '9'))
+			break;
+
+		if (*(s + count) == '-')
+			pn *= -1;
+
+		if ((*(s + count) >= '0') && (*(s + count) <= '9'))
+		{
+			if (size > 0)
+				m *= 10;
+			size++;
+		}
+		count++;
+	}
+
+	i = count - size;
+	while (i < count)
+	{
+		oi = oi + ((*(s + i) - 48) * m);
+		m /= 10;
+		i++;
+	}
+	return (oi * pn);
+}
