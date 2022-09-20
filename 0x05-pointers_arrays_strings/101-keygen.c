54c7029eb4fe21bab25614168c4d9e08b7a0dca4
marklwembe <marklwembe1@gmail.com>
2022-09-20 03:52:16 +0300

    Update 101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -16,15 +16,12 @@ void puts_half(char *str)
 	}
 
 	if (count % 2 == 1)
-
 		i = count / 2;
 
 	else
-
 		i = (count - 1) / 2;
 
 	for (i++; i < count; i++)
-
 		_putchar(str[i]);
 
 	_putchar('\n');
