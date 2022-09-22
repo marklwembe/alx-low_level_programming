71cfbb11f482a79a4ca4e8741a6549f2dff5a432
marklwembe <marklwembe1@gmail.com>
2022-09-23 02:56:54 +0300

    Task 5. Function that changes lower case letters to upper

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ *string_toupper - Change lowercase letters to upper.
+ *@a: Pointer to string.
+ *Return: Pointer to new string.
+ */
+char *string_toupper(char *a)
+{
+	int count = 0;
+	
+	while (a[count] != '\0')
+	{
+		if (a[count] >= 97 && a[count] <= 122)
+		{
+			a[count] = a[count] - 32;
+		}
+		count++;
+	}
+	return (a);
+}
