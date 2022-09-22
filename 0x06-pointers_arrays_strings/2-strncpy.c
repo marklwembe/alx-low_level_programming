3eabd7959c4fd4a1482ce5fbc01b638435f2d229
marklwembe <marklwembe1@gmail.com>
2022-09-22 13:04:18 +0300

    Task 2. Copy string from one array to another.

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _strncpy - Copies a given number of bytes.
+ * @dest: Copy destination
+ * @src: Copy source
+ * Return: Value referenced by @dest pointer
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+
+	while(i < n && src[i] != '\0')
+	{
+		dest[i] = src[i];
+		++i;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
