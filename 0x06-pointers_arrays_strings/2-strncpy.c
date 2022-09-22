6045b7d40001587a7ecacb9ccf65077b20316365
marklwembe <marklwembe1@gmail.com>
2022-09-22 13:28:16 +0300

    Update 2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -3,6 +3,7 @@
  * _strncpy - Copies a given number of bytes.
  * @dest: Copy destination
  * @src: Copy source
+ * @n: Number of characters to be copied
  * Return: Value referenced by @dest pointer
  */
 
@@ -10,7 +11,7 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int i = 0;
 
-	while(i < n && src[i] != '\0')
+	while (i < n && src[i] != '\0')
 	{
 		dest[i] = src[i];
 		++i;
