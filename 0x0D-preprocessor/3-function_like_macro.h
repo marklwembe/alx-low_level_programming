2a292b72fbf16072a6abd883328681f0c8242952
Mark Lwembe <marklwembe1@gmail.com>
2022-10-11 16:51:39 -0700

    Updated 3-function_like_macro.h

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,7 +1,7 @@
-#ifndef FUNCTION_LIKE_MACRO_H
-#define FUNCTION_LIKE_MACRO_H
+#ifndef FUNCTIONS
+#define FUNCTIONS
 
-#define ABS(x) ((x) < 0 ? (-x) : (x))
+#define ABS(x) (((x) < 0) ? (-(x)) : (x))
 
 #endif
 
