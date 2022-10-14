5c0678548b22869e5888494c6d4ef14b9379a508
marklwembe <marklwembe1@gmail.com>
2022-10-15 02:30:11 +0300

    Update 4-sum.h

--- 0x0D-preprocessor/4-sum.h
+++ 0x0D-preprocessor/4-sum.h
@@ -1,15 +1,6 @@
-<<<<<<< HEAD
-#ifndef MORE_FUNCTIONS
-#define MORE_FUNCTIONS
-=======
-#ifndef SUM_H
-#define SUM_H
->>>>>>> bb34a3d875b4d24603e542e60f4c11171c44e7ad
+#ifndef SUM_FUNCTION
+#define SUM_FUNCTION
 
 #define SUM(x, y) ((x) + (y))
 
 #endif
-<<<<<<< HEAD
-
-=======
->>>>>>> bb34a3d875b4d24603e542e60f4c11171c44e7ad
