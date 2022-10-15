337e7773e82aaca81a58eb599e487daf6359e1d8
marklwembe <marklwembe1@gmail.com>
2022-10-15 03:58:21 +0300

    Dynamite

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -10,5 +10,9 @@ int _putchar(char c);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int);
+void _memcpy(void *dest, void *src, size_t n);
+void _puts(char *str);
+void print_int(unsigned long int n);
+int _atoi(const char *s);
 
 #endif /* MAIN_H */
